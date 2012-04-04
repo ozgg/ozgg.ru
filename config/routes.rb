fd13e398495bfb0b49d4abb4bbcdb222b08fe960
Maxim Khan-Magomedov <dr.von.ozgg@gmail.com>
2012-04-05 01:08:03 +0400

    Добавил контроллер

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Ozgg::Application.routes.draw do
+  get "index/home"
+
+  get "index/about"
+
   resources :posts
 
   # The priority is based upon order of creation:
@@ -50,7 +54,7 @@ Ozgg::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'index#home'
 
   # See how all your routes lay out with "rake routes"
 
