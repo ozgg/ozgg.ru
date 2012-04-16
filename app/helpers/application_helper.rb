8e87e122e64c9dc658e2ec4b3d3a9283511deeff
Maxim Khan-Magomedov <dr.von.ozgg@gmail.com>
2012-04-16 20:36:55 +0400

    fix #4, добавил локализацию.
    see #1, добавил помощник преобразования тела записи.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def process_entry_body(body)
+    '<p>' + body.gsub("\n", '</p><p>') + '</p>'
+  end
 end
---
+++ config/initializers/i18n.rb
@@ -0,0 +1,6 @@
+#encoding: utf-8
+I18n.default_locale = :ru
+LANGUAGES = [
+  %w(English en),
+  %w(Русский ru)
+]
