79791ee2533fad434643b0a23a00f971106d8c0e
Maxim Khan-Magomedov <maxim.km@gmail.com>
2017-11-05 02:41:27 +0300

    Updated admin dashboard

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 Rails.application.config.assets.precompile << %w(admin.scss)
 Rails.application.config.assets.precompile << %w(biovision/base/**/*)
+Rails.application.config.assets.precompile << %w(biovision/post/**/*)
