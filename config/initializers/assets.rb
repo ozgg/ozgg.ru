3ecc97c0359da12b4b80111f0b6eb9a9ef4ef49c
Maxim Khan-Magomedov <maxim.km@gmail.com>
2018-08-06 14:57:40 +0300

    Reset to rails 5.2 with webpacker

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-Rails.application.config.assets.precompile << %w(admin.scss)
-Rails.application.config.assets.precompile << %w(biovision/base/**/*)
-Rails.application.config.assets.precompile << %w(biovision/post/*)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   root 'index#index'
-
-  mount Biovision::Base::Engine, at: '/'
 end
